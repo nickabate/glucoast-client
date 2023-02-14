0cdac06ae554d1588574eaeb1f1fe5e4915c61d0
Nick Abate <nick.abate@outlook.com>
2023-02-14 17:11:21 -0500

    Update responsiveness throughout.

--- src/components/HistoryBar/HistoryBar.js
+++ src/components/HistoryBar/HistoryBar.js
@@ -39,30 +39,32 @@ export default function HistoryBar({ allWeeks }) {
       </section> */}
       <section className="history">
         <h2 className="history__header">HISTORY</h2>
-        <NavLink className="history__link" to="/1">
-          Week 1
-        </NavLink>
-        <NavLink className="history__link" to="/2">
-          Week 2
-        </NavLink>
-        <NavLink className="history__link" to="/3">
-          Week 3
-        </NavLink>
-        <NavLink className="history__link" to="/4">
-          Week 4
-        </NavLink>
-        <NavLink className="history__link" to="/5">
-          Week 5
-        </NavLink>
-        <NavLink className="history__link" to="/6">
-          Week 6
-        </NavLink>
-        <NavLink className="history__link" to="/7">
-          Week 7
-        </NavLink>
-        <NavLink className="history__link" to="/8">
-          Week 8
-        </NavLink>
+        <div className="history__weeks">
+          <NavLink className="history__link" to="/1">
+            Week 1
+          </NavLink>
+          <NavLink className="history__link" to="/2">
+            Week 2
+          </NavLink>
+          <NavLink className="history__link" to="/3">
+            Week 3
+          </NavLink>
+          <NavLink className="history__link" to="/4">
+            Week 4
+          </NavLink>
+          <NavLink className="history__link" to="/5">
+            Week 5
+          </NavLink>
+          <NavLink className="history__link" to="/6">
+            Week 6
+          </NavLink>
+          <NavLink className="history__link" to="/7">
+            Week 7
+          </NavLink>
+          <NavLink className="history__link" to="/8">
+            Week 8
+          </NavLink>
+        </div>
       </section>
     </>
   );
